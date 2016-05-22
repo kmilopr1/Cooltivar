5f6c7227157fd1ac7347126c824d5a7303e929c8
kmilopr <trafalgar.cp@gmail.com>
2016-05-22 03:27:20 -0500

    stuff

--- app/src/main/java/com/hack/cooltivar/services/TemperatureCaptureService.java
+++ app/src/main/java/com/hack/cooltivar/services/TemperatureCaptureService.java
@@ -1,6 +1,7 @@
 package com.hack.cooltivar.services;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Service;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,9 +10,13 @@ import android.os.IBinder;
 import android.os.Process;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.hack.cooltivar.R;
 import com.hack.cooltivar.views.MainActivity;
 
 import java.text.SimpleDateFormat;
@@ -25,7 +30,7 @@ import java.util.TimerTask;
 public class TemperatureCaptureService extends Service {
     // constant
     public static final long NOTIFY_INTERVAL = 10 * 1000; // 10 seconds
-
+    private Button buttonAceptar;
     // run on another Thread to avoid crash
     private Handler mHandler = new Handler();
     // timer handling
@@ -64,7 +69,7 @@ public class TemperatureCaptureService extends Service {
                 @Override
                 public void run() {
                     // display toast
-
+/*
                     AlertDialog.Builder builder = new AlertDialog.Builder(TemperatureCaptureService.this);
                     builder.setTitle("Test dialog");
                     builder.setMessage("Content");
@@ -88,8 +93,30 @@ public class TemperatureCaptureService extends Service {
                         }
                     });
                     AlertDialog alert = builder.create();
+//                    alert.setContentView(R.layout.alert_cooltivar);
                     alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
                     alert.show();
+
+
+                    */
+
+                    final Dialog dialog = new Dialog(TemperatureCaptureService.this);
+                    dialog.setContentView(R.layout.alert_cooltivar);
+                    dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+                    dialog.show();
+
+                    buttonAceptar = (Button) dialog.findViewById(R.id.buttonAceptar);
+                    buttonAceptar.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i = new Intent(TemperatureCaptureService.this, MainActivity.class);
+                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(i);
+                            dialog.dismiss();
+                            Process.killProcess(Process.myPid());
+                        }
+                    });
+
 
                 }
 
--- app/src/main/java/com/hack/cooltivar/views/MainActivity.java
+++ app/src/main/java/com/hack/cooltivar/views/MainActivity.java
@@ -1,9 +1,11 @@
 package com.hack.cooltivar.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.hack.cooltivar.R;
+import com.hack.cooltivar.services.TemperatureCaptureService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Intent temperatureServiceStartAction = new Intent();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        startService(new Intent(this, TemperatureCaptureService.class));
     }
 }
